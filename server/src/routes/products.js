caa5f5238255d4872697f318e109b76ee6de53c7
Mark-Muchiri <muriithimac@gmail.com>
2023-04-20 13:08:12 +0300

    fwvvwfwefwefwe

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import { ProductModel } from "../models/Products.js";
 import { UserModel } from "../models/Users.js";
+import { verifyToken } from "./users.js"
 
 const router = express.Router();
 
@@ -15,7 +16,7 @@ router.get("/", async (req, res) => {
 });
 
 
-router.post("/", async (req, res) => {
+router.post("/", verifyToken, async (req, res) => {
    const product = new ProductModel(req.body);
    try {
       const response = await product.save();
@@ -26,7 +27,7 @@ router.post("/", async (req, res) => {
 });
 
 
-router.put("/", async (req, res) => {
+router.put("/", verifyToken, async (req, res) => {
    try {
       const product = await ProductModel.findById(req.body.productID);
       const user = await UserModel.findById(req.body.userID);
@@ -62,3 +63,4 @@ router.get("/savedProducts/:userID", async (req, res) => {
 });
 
 export { router as productsRouter };
+
